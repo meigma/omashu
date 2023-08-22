311090a2b8d63d07926a36b68f3d8695a8663859
Joshua Gilman <joshuagilman@gmail.com>
2023-08-22 14:32:26 -0700

    build: adds nix devenv shell

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Omashu";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = ["x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devenv.shells.default = {
+          name = "Omashu";
+          packages = with pkgs; [
+            nodejs_20
+          ];
+          enterShell = ''
+            echo "Welcome to the Omashu development environment!"
+          '';
+        };
+      };
+    };
+}
