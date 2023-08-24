b6e318a03fc47d5c3e96a70f0393ed933e819446
Joshua Gilman <joshuagilman@gmail.com>
2023-08-23 22:11:50 -0700

    feat: adds setup action

---
+++ actions/setup/src/main.ts
@@ -0,0 +1,26 @@
+import * as core from "@actions/core"
+import * as github from "@actions/github"
+import * as tc from '@actions/tool-cache';
+
+const baseURL = 'https://github.com/meigma/omashu/releases/download/v%VER%/omashu-%VER%-linux_amd64.tar.gz'
+
+export async function run() {
+    const version = core.getInput('version')
+
+    if (!isSemVer(version)) {
+        core.setFailed('Invalid version')
+        return
+    }
+
+    const finalURL = baseURL.replace(/%VER%/g, version)
+    if (process.platform == 'linux') {
+        const downloadPath = await tc.downloadTool(finalURL)
+        await tc.extractTar(downloadPath, '/usr/local/bin')
+    } else {
+        core.setFailed('Unsupported platform')
+    }
+}
+
+function isSemVer(version: string): boolean {
+    return /^\d+\.\d+\.\d+$/.test(version)
+}
