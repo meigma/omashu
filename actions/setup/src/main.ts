e4385ad9f803dab84179fac84cd16d9a16bac9fa
Joshua Gilman <joshuagilman@gmail.com>
2023-08-24 13:32:58 -0700

    test: adds tests for setup action

--- actions/setup/src/main.ts
+++ actions/setup/src/main.ts
@@ -1,26 +1,33 @@
 import * as core from "@actions/core"
-import * as github from "@actions/github"
 import * as tc from '@actions/tool-cache';
 
 const baseURL = 'https://github.com/meigma/omashu/releases/download/v%VER%/omashu-%VER%-linux_amd64.tar.gz'
 
 export async function run() {
-    const version = core.getInput('version')
+    try {
+        const version = core.getInput('version')
 
-    if (!isSemVer(version)) {
-        core.setFailed('Invalid version')
-        return
-    }
+        if (!isSemVer(version)) {
+            core.setFailed('Invalid version')
+            return
+        }
 
-    const finalURL = baseURL.replace(/%VER%/g, version)
-    core.info(`Downlaoading version ${version} from ${finalURL}`)
-    if (process.platform == 'linux') {
-        const downloadPath = await tc.downloadTool(finalURL)
-        const extractPath = await tc.extractTar(downloadPath, '/usr/local/bin')
+        const finalURL = baseURL.replace(/%VER%/g, version)
+        core.info(`Downlaoading version ${version} from ${finalURL}`)
+        if (process.platform == 'linux') {
+            const downloadPath = await tc.downloadTool(finalURL)
+            const extractPath = await tc.extractTar(downloadPath, '/usr/local/bin')
 
-        core.info(`Installed omashu to ${extractPath}`)
-    } else {
-        core.setFailed('Unsupported platform')
+            core.info(`Installed omashu to ${extractPath}`)
+        } else {
+            core.setFailed('Unsupported platform')
+        }
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message)
+        } else {
+            core.setFailed('Unknown error')
+        }
     }
 }
 
